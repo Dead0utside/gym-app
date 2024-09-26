817a9c0b7b07567881c57505188493c3d1488e24
Dead0utside <volodya1234321@gmail.com>
2024-09-26 21:25:09 +0200

    add java part

---
+++ poco-project/src/test/java/com/dead0uts1de/poco_project/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dead0uts1de.poco_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
