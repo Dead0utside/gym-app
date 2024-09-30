c5e82362ed403b4689b0d43d0be448241bab568b
Dead0utside <volodya1234321@gmail.com>
2024-09-30 16:28:10 +0200

    my pc shits itself again. This is supposed to work but it doesn't

--- client/src/components/MyComponent.tsx
+++ client/src/components/MyComponent.tsx
@@ -1,6 +1,36 @@
+import fetch from 'node-fetch';
+import { useState, useEffect } from 'react';
+
+const GET_URL = "http://localhost:8080/api/v1/hello/first";
+interface HelloObject {
+  id: number;
+  message: string;
+}
+
 function MyComponent() {
+  const [hello, setHello] = useState("");
+
+  // useEffect(() => {
+  //   const fetchHello = async () => {
+  //     const response = await fetch(`${GET_URL}`);
+  //     const responseJson = (await response.json()) as HelloObject;
+
+  //     console.log(responseJson.message);
+
+  //     setHello(responseJson.message);
+  //   }
+
+  //   fetchHello();
+  // }, []);
+
+  useEffect(() => {
+    fetch(`${GET_URL}`)
+    .then(response => response.json)
+    .then(responseJson => console.log(responseJson));
+  }, []);
+
   return (
-    <div>Hello World</div>
+    <div>{hello}</div>
   )
 }
 
