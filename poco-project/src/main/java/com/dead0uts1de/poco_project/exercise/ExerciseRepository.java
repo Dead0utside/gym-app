cc12f95ba630d139ffbbdfaad114001a87fa7685
Volodymyr <volodya1234321@gmail.com>
2024-10-04 09:47:53 +0200

    add ExerciseRepository

---
+++ poco-project/src/main/java/com/dead0uts1de/poco_project/exercise/ExerciseRepository.java
@@ -0,0 +1,16 @@
+package com.dead0uts1de.poco_project.exercise;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+    @Override
+    @NonNull
+    Optional<Exercise> findById(@NonNull Long id);
+
+}
