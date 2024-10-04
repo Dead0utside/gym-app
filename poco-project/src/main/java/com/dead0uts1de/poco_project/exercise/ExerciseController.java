6621e4597670ef195d7c5cf06db3d92dbeb02c03
Volodymyr <volodya1234321@gmail.com>
2024-10-04 11:13:55 +0200

    start exercise controller. Not functional yet

---
+++ poco-project/src/main/java/com/dead0uts1de/poco_project/exercise/ExerciseController.java
@@ -0,0 +1,21 @@
+package com.dead0uts1de.poco_project.exercise;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/exercise")
+@RequiredArgsConstructor
+public class ExerciseController {
+    private final ExerciseService exerciseService;
+
+    @Value("${corsHeader}")
+    private String corsHeader;
+
+    public ResponseEntity<Exercise> getExerciseById(Long id) {
+        return null;
+    }
+}
