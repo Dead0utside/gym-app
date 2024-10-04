b981099818051f80dab9cc9c7d273a1ba11d1a72
Volodymyr <volodya1234321@gmail.com>
2024-10-04 10:08:58 +0200

    add ExerciseService

---
+++ poco-project/src/main/java/com/dead0uts1de/poco_project/exercise/ExerciseService.java
@@ -0,0 +1,15 @@
+package com.dead0uts1de.poco_project.exercise;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExerciseService {
+    private final ExerciseRepository exerciseRepository;
+
+    public Exercise findExerciseById(Long id) {
+        return exerciseRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Exercise with id " + id + " not found"));
+    }
+}
