09bbf5ce8bac38bcde2e801560bad3d0c461d438
Volodymyr <volodya1234321@gmail.com>
2024-10-04 12:21:02 +0200

    Remove custom response entity (couldn't figure it out)

--- poco-project/src/main/java/com/dead0uts1de/pocoProject/config/CustomResponseEntity.java
+++
@@ -1,9 +0,0 @@
-package com.dead0uts1de.pocoProject.config;
-
-import org.springframework.http.ResponseEntity;
-
-public class CustomResponseEntity {
-    public <T> ResponseEntity<T> OkResponse(T body) {
-        return ResponseEntity.ok().header("Access-Control-Allow-Origin", "*").body(body);
-    }
-}
