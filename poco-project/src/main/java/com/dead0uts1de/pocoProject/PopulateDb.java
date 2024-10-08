63d8147b7b82741d4830bb7ab1a39562b4c949cc
Volodymyr <volodya1234321@gmail.com>
2024-10-08 16:03:09 +0200

    create a new exercise and add it to DB

--- poco-project/src/main/java/com/dead0uts1de/pocoProject/PopulateDb.java
+++ poco-project/src/main/java/com/dead0uts1de/pocoProject/PopulateDb.java
@@ -1,5 +1,8 @@
 package com.dead0uts1de.pocoProject;
 
+import com.dead0uts1de.pocoProject.exercise.Exercise;
+import com.dead0uts1de.pocoProject.exercise.ExerciseController;
+import com.dead0uts1de.pocoProject.exercise.ExerciseService;
 import com.dead0uts1de.pocoProject.hello.Hello;
 import com.dead0uts1de.pocoProject.hello.HelloService;
 import org.springframework.boot.CommandLineRunner;
@@ -9,10 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PopulateDb {
     @Bean
-    CommandLineRunner HelloCLR(HelloService helloService) {
+    CommandLineRunner HelloCLR(HelloService helloService, ExerciseService exerciseService, ExerciseController exerciseController) {
         return args -> {
             Hello hello = new Hello("Hello World");
             helloService.addHello(hello);
+
+            Exercise ex = new Exercise("bench press", 55f, 3, 12);
+            exerciseController.addExercise(ex);
         };
     }
 }
