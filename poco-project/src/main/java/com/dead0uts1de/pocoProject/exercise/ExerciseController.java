afc112238b496c51cfcff90513e1e93498b1099d
Volodymyr <volodya1234321@gmail.com>
2024-10-08 16:01:27 +0200

    add a get mapping for getExerciseById(); add new endpoint for Post request to add exercise

--- poco-project/src/main/java/com/dead0uts1de/pocoProject/exercise/ExerciseController.java
+++ poco-project/src/main/java/com/dead0uts1de/pocoProject/exercise/ExerciseController.java
@@ -1,9 +1,9 @@
 package com.dead0uts1de.pocoProject.exercise;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/exercise")
@@ -11,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExerciseController {
     private final ExerciseService exerciseService;
 
-    public ResponseEntity<Exercise> getExerciseById(Long id) {
+    @GetMapping(path = "/get/{exerciseId}")
+    public ResponseEntity<Exercise> getExerciseById(@PathVariable(value = "exerciseId") Long id) {
         return ResponseEntity.ok()
                 .header("Access-Control-Allow-Origin", "*")
                 .body(exerciseService.getExerciseById(id));
     }
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<String> addExercise(@RequestBody Exercise exercise) {
+        exerciseService.addExercise(exercise);
+        return new ResponseEntity<>("Add exercise", HttpStatus.OK);
+    }
+
 }
