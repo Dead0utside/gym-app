45e9885189dea995959d2e14e49efe5a6eb1b153
Volodymyr <volodya1234321@gmail.com>
2024-10-11 16:03:56 +0200

    create training repository and service

---
+++ poco-project/src/main/java/com/dead0uts1de/pocoProject/training/TrainingRepository.java
@@ -0,0 +1,14 @@
+package com.dead0uts1de.pocoProject.training;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TrainingRepository extends JpaRepository<Training, Long> {
+    @Override
+    @NonNull
+    Optional<Training> findById(@NonNull Long id);
+}
