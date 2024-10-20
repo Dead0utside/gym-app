7877284a151543acf6ae959a74d148e918b07f01
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-20 17:36:26 +0200

    add theme selection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Sidebar from "@/components/elements/sidebar";
+import { ThemeProvider } from "./components/utilities/themeProvider";
 
 Button;
 
 function App() {
 	return (
-		<div className="app">
-			<Sidebar></Sidebar>
-		</div>
+		<ThemeProvider>
+			<div className="app">
+				<Sidebar />
+			</div>
+		</ThemeProvider>
 	);
 }
 
--- client/src/components/elements/sidebar/index.tsx
+++ client/src/components/elements/sidebar/index.tsx
@@ -1,12 +1,15 @@
+import { ModeToggle } from "@/components/utilities/modeToggle";
+
 const Sidebar = () => {
 	return (
-		<aside className="h-screen w-1/6 bg-zinc-400">
-			<header className="flex border-b-2 border-solid font-sans">
+		<aside className="h-screen w-1/6 bg-zinc-800">
+			<header className="flex border-b-2 border-solid">
 				{/* <p className="mx-auto">Trainings</p> */}
-				<h1 className="scroll-m-20 text-4xl tracking-tight lg:text-5xl">
-					Taxing Laughter: The Joke Tax Chronicles
+				<h1 className="mx-auto my-2 scroll-m-20 font-sans text-zinc-400">
+					trainings
 				</h1>
 			</header>
+			<ModeToggle />
 		</aside>
 	);
 };
