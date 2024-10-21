fb8ba3eb0f0ede15ec71378afcc0db003267e83b
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-21 11:10:11 +0200

    add getTrainingById method to Training Service

--- poco-project/src/main/java/com/dead0uts1de/pocoProject/training/TrainingService.java
+++ poco-project/src/main/java/com/dead0uts1de/pocoProject/training/TrainingService.java
@@ -1,8 +1,16 @@
 package com.dead0uts1de.pocoProject.training;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TrainingService {
+    private final TrainingRepository trainingRepository;
+
+    public Training getTrainingById(Long id) {
+        return trainingRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("training with id " + id + " not found"));
+    }
 
 }
