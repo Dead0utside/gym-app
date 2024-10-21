1ba0eeb9cfa85eecf02c74e82e94347e1c2ccdd9
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-21 12:31:22 +0200

    refactor ExerciseController.getExerciseById

--- poco-project/src/main/java/com/dead0uts1de/pocoProject/exercise/ExerciseController.java
+++ poco-project/src/main/java/com/dead0uts1de/pocoProject/exercise/ExerciseController.java
@@ -1,21 +1,32 @@
 package com.dead0uts1de.pocoProject.exercise;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/exercise")
-@RequiredArgsConstructor
 public class ExerciseController {
     private final ExerciseService exerciseService;
+    private final HttpHeaders headers = new HttpHeaders();
+
+    public ExerciseController(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        headers.add("Access-Control-Allow-Origin", "*");
+    }
 
     @GetMapping(path = "/get/{exerciseId}")
     public ResponseEntity<Exercise> getExerciseById(@PathVariable(value = "exerciseId") Long id) {
-        return ResponseEntity.ok()
-                .header("Access-Control-Allow-Origin", "*")
-                .body(exerciseService.getExerciseById(id));
+        Exercise result;
+        try {
+            result = exerciseService.getExerciseById(id);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(headers, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(result, headers, HttpStatus.OK);
     }
 
     @PostMapping(path = "/add")
