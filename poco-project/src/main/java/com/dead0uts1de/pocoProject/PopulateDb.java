adbb211bd5a4525a2681a809e3ce5c654857016e
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-21 13:40:59 +0200

    populate db with a training

--- poco-project/src/main/java/com/dead0uts1de/pocoProject/PopulateDb.java
+++ poco-project/src/main/java/com/dead0uts1de/pocoProject/PopulateDb.java
@@ -2,9 +2,10 @@ package com.dead0uts1de.pocoProject;
 
 import com.dead0uts1de.pocoProject.exercise.Exercise;
 import com.dead0uts1de.pocoProject.exercise.ExerciseController;
-import com.dead0uts1de.pocoProject.exercise.ExerciseService;
 import com.dead0uts1de.pocoProject.hello.Hello;
 import com.dead0uts1de.pocoProject.hello.HelloService;
+import com.dead0uts1de.pocoProject.training.Training;
+import com.dead0uts1de.pocoProject.training.TrainingController;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +13,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PopulateDb {
     @Bean
-    CommandLineRunner HelloCLR(HelloService helloService, ExerciseService exerciseService, ExerciseController exerciseController) {
+    CommandLineRunner HelloCLR(HelloService helloService, ExerciseController exerciseController, TrainingController trainingController) {
         return args -> {
             Hello hello = new Hello("Hello World");
             helloService.addHello(hello);
 
             Exercise ex = new Exercise("bench press", 55f, 3, 12);
             exerciseController.addExercise(ex);
+
+            Training training1 = new Training("monday");
+            trainingController.addTraining(training1);
+
         };
     }
 }
