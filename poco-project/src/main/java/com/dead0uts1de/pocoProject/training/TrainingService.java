6b3e68b7c3a98bdb19db3658ba8dc9f23ba43f7e
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-21 11:17:11 +0200

    add find all to training service

--- poco-project/src/main/java/com/dead0uts1de/pocoProject/training/TrainingService.java
+++ poco-project/src/main/java/com/dead0uts1de/pocoProject/training/TrainingService.java
@@ -3,6 +3,8 @@ package com.dead0uts1de.pocoProject.training;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TrainingService {
@@ -11,6 +13,10 @@ public class TrainingService {
     public Training getTrainingById(Long id) {
         return trainingRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("training with id " + id + " not found"));
+    }
+
+    public List<Training> getAllTrainings() {
+        return trainingRepository.findAll();
     }
 
 }
