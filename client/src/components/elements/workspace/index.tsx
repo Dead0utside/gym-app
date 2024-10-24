4f2a5b6ac0aaa102f7ff8b006f07b9c501280185
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-24 15:53:17 +0200

    add workspace component

---
+++ client/src/components/elements/workspace/index.tsx
@@ -0,0 +1,11 @@
+import { Training } from "@/components/utilities/types";
+
+type Props = {
+	trainingName: Training;
+};
+
+const index = ({ trainingName }: Props) => {
+	return <div>Workspace</div>;
+};
+
+export default index;
