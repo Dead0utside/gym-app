645c5ca3ac451d4af9eb89294c55412a8c3aa5f7
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-30 19:49:09 +0100

    add a dialog for training creation

--- client/src/components/elements/sidebar/index.tsx
+++ client/src/components/elements/sidebar/index.tsx
@@ -16,6 +16,15 @@ import { Training } from "@/components/utilities/types";
 import { Plus } from "lucide-react";
 import { useEffect, useState } from "react";
 import TrainingMenuItem from "@/components/elements/sidebar/trainingMenuItem.tsx";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription, DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from "@/components/ui/dialog.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 const GET_URL = "http://localhost:8080/api/v1/training/get";
 
@@ -47,9 +56,24 @@ export function AppSidebar({ setWorkspaceContent }: Props) {
 			<SidebarContent>
 				<SidebarGroup>
 					<SidebarGroupLabel>Trainings</SidebarGroupLabel>
-					<SidebarGroupAction title="Add Training">
-						<Plus /> <span className="sr-only"> Add Training</span>
-					</SidebarGroupAction>
+
+					<Dialog>
+						<DialogTrigger asChild>
+							<SidebarGroupAction title="Add Training">
+								<Plus /> <span className="sr-only"> Add Training</span>
+							</SidebarGroupAction>
+						</DialogTrigger>
+						<DialogContent>
+							<DialogHeader>
+								<DialogTitle>Add training</DialogTitle>
+								<DialogDescription>Create a new training</DialogDescription>
+							</DialogHeader>
+							// TODO create a form for training creation
+							<DialogFooter>
+								<Button type={"submit"}> Add</Button>
+							</DialogFooter>
+						</DialogContent>
+					</Dialog>
 					<SidebarGroupContent>
 						<SidebarMenu>
 							{trainings.map((training) => (
